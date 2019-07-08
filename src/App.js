cbfc117f9df8cd9f813853cea577eed09373d89e
RJ Salamanca <salamancaronito@gmail.com>
2019-07-08 09:40:15 -0400

    added different route displays

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routue, Link } from 'react-router-dom';
+
 import './App.css';
 
+const Index = () => <h2>Home Page</h2>;
+const About = () => <h2>About Page</h2>
+const Contact = () => <h2>Contact Page</h2>;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App" />
 }
 
 export default App;
