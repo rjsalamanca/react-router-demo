0ee157697059d8085e4001619d1b00ddd75b81a8
RJ Salamanca <salamancaronito@gmail.com>
2019-07-08 09:48:51 -0400

    added links on link click display a component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Routue, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import './App.css';
 
+// Component
 const Index = () => <h2>Home Page</h2>;
 const About = () => <h2>About Page</h2>
 const Contact = () => <h2>Contact Page</h2>;
 
 function App() {
-  return <div className="App" />
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home Page</Link>
+            </li>
+            <li>
+              <Link to='/about'>About Page</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact Page</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" exact component={Index} />
+        <Route path="/about" exact component={About} />
+        <Route path="/contact" exact component={Contact} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
